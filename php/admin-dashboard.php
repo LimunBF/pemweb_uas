15eec08eeaa585a62357e0c8fc43ebabd4fcaf32
Refa Hilyah <refahilyaa@student.uns.ac.id>
2024-12-21 17:39:23 +0700

    admin-dashboard part4

--- php/admin-dashboard.php
+++ php/admin-dashboard.php
@@ -31,7 +31,7 @@ if ($conn->connect_error) {
     <!-- Bootstrap 5.3 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-    <link rel="stylesheet" type="text/css" href="css/admin_dashboard.css">
+    <link rel="stylesheet" type="text/css" href="css/admin-dashboard.css">
 </head>
 
 <body>
